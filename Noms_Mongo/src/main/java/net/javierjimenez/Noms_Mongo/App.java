595924e8025420727b0a643c7db17feb2c9095e5
alumne1daw <alumne1daw@localhost>
2016-04-22 16:44:58 +0200

    Commit: Programa en progreso

--- Noms_Mongo/src/main/java/net/javierjimenez/Noms_Mongo/App.java
+++ Noms_Mongo/src/main/java/net/javierjimenez/Noms_Mongo/App.java
@@ -7,22 +7,47 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        MongoClient h = new MongoClient(new MongoClientURI("mongodb://admin:admin@ds017231.mlab.com:17231/noms_mongodb"));
-        
-        MongoDatabase i = h.getDatabase("noms_mongodb");
-        
-        MongoCollection<Document> j = i.getCollection("noms");
-        
-        h.close();
-        
-        System.out.print("fin");
-    }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/noms.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/noms.css").toExternalForm());
+			primaryStage.setTitle("Un altre cop noms!");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
+/*public class App {
+	public static void main(String[] args) {
+		MongoClient h = new MongoClient(
+				new MongoClientURI("mongodb://admin:admin@ds017231.mlab.com:17231/noms_mongodb"));
+
+		MongoDatabase i = h.getDatabase("noms_mongodb");
+
+		MongoCollection<Document> j = i.getCollection("noms");
+
+		h.close();
+
+		System.out.print("fin");
+	}
+}*/
