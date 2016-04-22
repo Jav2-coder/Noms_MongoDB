bd5835c0accf6a70e8843ec3aa7b0e481775f1e0
alumne1daw <alumne1daw@localhost>
2016-04-22 16:21:03 +0200

    Commit: Realizado enlace con MongoLab - Colección añadida

--- Noms_Mongo/src/main/java/net/javierjimenez/Noms_Mongo/App.java
+++ Noms_Mongo/src/main/java/net/javierjimenez/Noms_Mongo/App.java
@@ -1,5 +1,12 @@
 package net.javierjimenez.Noms_Mongo;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MongoClient h = new MongoClient(new MongoClientURI("mongodb://admin:admin@ds017231.mlab.com:17231/noms_mongodb"));
+        
+        MongoDatabase i = h.getDatabase("noms_mongodb");
+        
+        MongoCollection<Document> j = i.getCollection("noms");
+        
+        h.close();
+        
+        System.out.print("fin");
     }
 }
